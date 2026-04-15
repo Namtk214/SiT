ee6344aab1a19eafd152e4e79dc17547132885a4
Nam <nam.ngothanh05@gmail.com>
2026-04-15 09:51:09 +0700

    adding instructions

---
+++ SiT.py
@@ -0,0 +1,86 @@
+!git clone https://github.com/willisma/SiT.git
+import SiT, os
+os.chdir('SiT')
+os.environ['PYTHONPATH'] = '/env/python:/content/SiT'
+!pip install diffusers timm torchdiffeq --upgrade
+# SiT imports:
+import torch
+from torchvision.utils import save_image
+from transport import create_transport, Sampler
+from diffusers.models import AutoencoderKL
+from download import find_model
+from models import SiT_XL_2
+from PIL import Image
+from IPython.display import display
+torch.set_grad_enabled(False)
+device = "cuda" if torch.cuda.is_available() else "cpu"
+if device == "cpu":
+    print("GPU not found. Using CPU instead.")
+
+image_size = "256"
+vae_model = "stabilityai/sd-vae-ft-ema" #@param ["stabilityai/sd-vae-ft-mse", "stabilityai/sd-vae-ft-ema"]
+latent_size = int(image_size) // 8
+# Load model:
+model = SiT_XL_2(input_size=latent_size).to(device)
+state_dict = find_model(f"SiT-XL-2-{image_size}x{image_size}.pt")
+model.load_state_dict(state_dict)
+model.eval() # important!
+vae = AutoencoderKL.from_pretrained(vae_model).to(device)
+# Set user inputs:
+seed = 0 #@param {type:"number"}
+torch.manual_seed(seed)
+num_sampling_steps = 250 #@param {type:"slider", min:0, max:1000, step:1}
+cfg_scale = 4 #@param {type:"slider", min:1, max:10, step:0.1}
+class_labels = 207, 360, 387, 974, 88, 979, 417, 279 #@param {type:"raw"}
+samples_per_row = 4 #@param {type:"number"}
+sampler_type = "ODE" #@param ["ODE", "SDE"]
+
+
+# Create diffusion object:
+transport = create_transport()
+sampler = Sampler(transport)
+
+# Create sampling noise:
+n = len(class_labels)
+z = torch.randn(n, 4, latent_size, latent_size, device=device)
+y = torch.tensor(class_labels, device=device)
+
+# Setup classifier-free guidance:
+z = torch.cat([z, z], 0)
+y_null = torch.tensor([1000] * n, device=device)
+y = torch.cat([y, y_null], 0)
+model_kwargs = dict(y=y, cfg_scale=cfg_scale)
+
+# Sample images:
+if sampler_type == "SDE":
+    SDE_sampling_method = "Euler" #@param ["Euler", "Heun"]
+    diffusion_form = "linear" #@param ["constant", "SBDM", "sigma", "linear", "decreasing", "increasing-decreasing"]
+    diffusion_norm = 1 #@param {type:"slider", min:0, max:10.0, step:0.1}
+    last_step = "Mean" #@param ["Mean", "Tweedie", "Euler"]
+    last_step_size = 0.4 #@param {type:"slider", min:0, max:1.0, step:0.01}
+    sample_fn = sampler.sample_sde(
+        sampling_method=SDE_sampling_method,
+        diffusion_form=diffusion_form, 
+        diffusion_norm=diffusion_norm,
+        last_step_size=last_step_size, 
+        num_steps=num_sampling_steps,
+    ) 
+elif sampler_type == "ODE":
+    # default to Adaptive Solver
+    ODE_sampling_method = "dopri5" #@param ["dopri5", "euler", "rk4"]
+    atol = 1e-6
+    rtol = 1e-3
+    sample_fn = sampler.sample_ode(
+        sampling_method=ODE_sampling_method,
+        atol=atol,
+        rtol=rtol,
+        num_steps=num_sampling_steps
+    ) 
+samples = sample_fn(z, model.forward_with_cfg, **model_kwargs)[-1]
+samples = vae.decode(samples / 0.18215).sample
+
+# Save and display images:
+save_image(samples, "sample.png", nrow=int(samples_per_row), 
+           normalize=True, value_range=(-1, 1))
+samples = Image.open("sample.png")
+display(samples)
